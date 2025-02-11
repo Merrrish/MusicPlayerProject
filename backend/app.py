d57576e1dc38ad131fb163a70db837cf0ab06ead
Merrrish <isbro4217@gmail.com>
2025-02-11 18:55:44 +0100

    Обновил файлы фронтенда и бэкенда

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify
 from models import db, Song
 from flask_cors import CORS
 import json
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://music-player-project-coral.vercel.app"}})
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:hFsMLKrkCudSHhjgiZuOIQWjiPkjGQZK@autorack.proxy.rlwy.net:41982/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/api/songs', methods=['GET'])
 def get_songs():
